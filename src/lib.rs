a49ebc0e2d196e0866659248497b6bd5b750167c
Vladimir Gromov <v.dece1verr@hotmail.com>
2022-11-18 00:11:25 +0300

    about

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,25 @@ use crossterm::{
     event::{read, Event, KeyCode},
     execute,
     style::{Color, Print, ResetColor, SetForegroundColor},
-    terminal::disable_raw_mode,
+    terminal::{disable_raw_mode, Clear, ClearType},
 };
 use is_elevated::is_elevated;
-use std::io::{stdout, Read};
+use std::io::stdout;
 
 pub enum Command {
+    ShowAbout,
     Exit,
 }
 
 pub fn pause() {
-    let _ = std::io::stdin().read(&mut [0]).unwrap();
+    execute!(stdout(), Print("Нажмите любую клавишу для продолжения..."),).unwrap();
+    loop {
+        if let Event::Key(event) = read().unwrap() {
+            if let KeyCode::Char(_) = event.code {
+                break;
+            }
+        }
+    }
 }
 
 fn is_installed() -> bool {
@@ -20,16 +28,45 @@ fn is_installed() -> bool {
 }
 
 pub fn wait_command() -> Command {
+    clearscreen::clear().unwrap();
+    if is_elevated() {
+        execute!(
+            stdout(),
+            Print("Программа запущена с правами "),
+            SetForegroundColor(Color::Green),
+            Print("администратора"),
+            ResetColor,
+            Print(".\n"),
+        )
+        .unwrap();
+    } else {
+        execute!(
+            stdout(),
+            Print("Программа запущена c "),
+            SetForegroundColor(Color::Red),
+            Print("ограниченными"),
+            ResetColor,
+            Print(" правами.\n")
+        )
+        .unwrap();
+    }
+
     if is_elevated() && !is_installed() {
         execute!(
             stdout(),
-            Print("\n[1] Установить NC Renamer и добавить в контекстное меню."),
+            SetForegroundColor(Color::Yellow),
+            Print("\n[1]"),
+            ResetColor,
+            Print(" Удалить NC Renamer и убрать из контекстного меню. "),
         )
         .unwrap();
     } else if !is_elevated() && !is_installed() {
         execute!(
             stdout(),
-            Print("\n[1] Установить NC Renamer и добавить в контекстное меню. "),
+            SetForegroundColor(Color::Yellow),
+            Print("\n[1]"),
+            ResetColor,
+            Print(" Удалить NC Renamer и убрать из контекстного меню. "),
             SetForegroundColor(Color::Red),
             Print("(недоступно)"),
             ResetColor
@@ -38,13 +75,19 @@ pub fn wait_command() -> Command {
     } else if is_elevated() && is_installed() {
         execute!(
             stdout(),
-            Print("\n[1] Удалить NC Renamer и убрать из контекстного меню. "),
+            SetForegroundColor(Color::Yellow),
+            Print("\n[1]"),
+            ResetColor,
+            Print(" Удалить NC Renamer и убрать из контекстного меню. "),
         )
         .unwrap();
     } else {
         execute!(
             stdout(),
-            Print("\n[1] Удалить NC Renamer и убрать из контекстного меню. "),
+            SetForegroundColor(Color::Yellow),
+            Print("\n[1]"),
+            ResetColor,
+            Print(" Удалить NC Renamer и убрать из контекстного меню. "),
             SetForegroundColor(Color::Red),
             Print("(недоступно)"),
             ResetColor
@@ -52,7 +95,22 @@ pub fn wait_command() -> Command {
         .unwrap();
     }
 
-    execute!(stdout(), Print("\n\n[0] Закрыть программу. "),).unwrap();
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Yellow),
+        Print("\n[2]"),
+        ResetColor,
+        Print(" О программе "),
+    )
+    .unwrap();
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Yellow),
+        Print("\n\n[0]"),
+        ResetColor,
+        Print(" Закрыть программу"),
+    )
+    .unwrap();
 
     crossterm::terminal::enable_raw_mode().unwrap();
     let command;
@@ -63,6 +121,10 @@ pub fn wait_command() -> Command {
                     command = Command::Exit;
                     break;
                 }
+                KeyCode::Char('2') => {
+                    command = Command::ShowAbout;
+                    break;
+                }
                 //_ => println!("{:?}", event.code),
                 _ => (),
             }
@@ -70,4 +132,41 @@ pub fn wait_command() -> Command {
     }
     disable_raw_mode().unwrap();
     command
+}
+
+pub fn show_about() {
+    clearscreen::clear().unwrap();
+    execute!(stdout(), Clear(ClearType::All),).unwrap();
+    execute!(stdout(), Print("Программа переименовывает файлы управляющих программ по названию самой управляющей программы.\n
+Поддерживаемые СЧПУ:
+* Fanuc 0i           [ O0001(НАЗВАНИЕ) ]
+* Fanuc 0i-*F        [ <НАЗВАНИЕ> ]
+* Mazatrol Smart     [ .PBG | .PBD ]
+* Sinumerik 840D sl  [ MSG (\"НАЗВАНИЕ\") ]
+* Hiedenhain         [ BEGIN PGM НАЗВАНИЕ MM ]
+
+Использование программы подразумевается вызовом через контекстное меню (ПКМ) по переменовываемым файлам.
+Работа программы происходит без каких-либо уведомлений.
+Если программа определяет файл как УП, то происходит переименование.
+Если файл уже существует, он не перезаписывается, а создается копия с добавлением номера."),).unwrap();
+
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Yellow),
+        Print("\n\n[0]"),
+        ResetColor,
+        Print(" Назад"),
+    )
+    .unwrap();
+    loop {
+        if let Event::Key(event) = read().unwrap() {
+            match event.code {
+                KeyCode::Esc | KeyCode::Char('0') => {
+                    break;
+                }
+                //_ => println!("{:?}", event.code),
+                _ => (),
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,29 @@
 use crossterm::{
+    cursor::{Hide, Show},
     execute,
-    style::{Color, Print, ResetColor, SetForegroundColor},
-    terminal::SetTitle,
+    terminal::{Clear, ClearType, SetTitle},
     Result,
 };
-use is_elevated::is_elevated;
-use nc_renamer::wait_command;
+use nc_renamer::{show_about, wait_command};
 use std::io::stdout;
 
 fn main() -> Result<()> {
+    clearscreen::clear().unwrap();
     execute!(
         stdout(),
         SetTitle("NC Renamer"),
-        Print("Программа запущена с ")
+        Hide,
+        Clear(ClearType::All)
     )?;
-    if is_elevated() {
-        execute!(
-            stdout(),
-            Print("правами "),
-            SetForegroundColor(Color::Green),
-            Print("администратора"),
-            ResetColor,
-            Print(".\n"),
-        )?;
-    } else {
-        execute!(
-            stdout(),
-            SetForegroundColor(Color::Red),
-            Print("ограниченными"),
-            ResetColor,
-            Print(" правами.\n")
-        )?;
-    }
 
-    match wait_command() {
-        nc_renamer::Command::Exit => {}
+    loop {
+        match wait_command() {
+            nc_renamer::Command::Exit => break,
+            nc_renamer::Command::ShowAbout => {
+                show_about();
+            }
+        }
     }
-
+    execute!(stdout(), Show,)?;
     Ok(())
 }
